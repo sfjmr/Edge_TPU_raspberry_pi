4e231b48e47b80d34f8a0aef580b2d12ecdf1eff
kawasemiwho <fujimori.shu.sf@gmail.com>
2019-04-11 17:09:39 +0900

    fix

--- python-tflite-source/edgetpu/demo/detection_capture.py
+++ python-tflite-source/edgetpu/demo/detection_capture.py
@@ -24,10 +24,11 @@ def main():
     #engine = edgetpu.classification.engine.ClassificationEngine(args.model)
     engine = edgetpu.detection.engine.DetectionEngine(args.model)
     print("engine.required_input_array_size()")
-    print(engine.required_input_array_size())
+    print(engine.required_input_array_size()) #270000
     with picamera.PiCamera() as camera:
         #camera.resolution = (640, 480)
-        camera.resolution = (300, 300)
+        #300 300 291840
+        camera.resolution = (270, 270)
         camera.framerate = 30
         _, width, height, channels = engine.get_input_tensor_shape()
         print("width, height, channels")
