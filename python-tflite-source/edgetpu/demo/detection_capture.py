f029ae389677b4f52dbb0dc6fb623bf639507a8a
kawasemiwho <fujimori.shu.sf@gmail.com>
2019-04-12 11:17:38 +0900

    chg resize

--- python-tflite-source/edgetpu/demo/detection_capture.py
+++ python-tflite-source/edgetpu/demo/detection_capture.py
@@ -40,9 +40,9 @@ def main():
             for foo in camera.capture_continuous(stream,
                                                  format='rgb',
                                                  use_video_port=True,
-                                                 resize=(250, 250)):#うまくresizeされてないのが問題?
+                                                 resize=(255, 255)):#うまくresizeされてないのが問題?
                 #300 300 -> 291840
-                #250 250 -> 
+                #250 250 -> 196608
                 #200 200 -> 139776
                 stream.truncate()
                 stream.seek(0)
